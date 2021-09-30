9c4df15a16987a3968985cf20273ec34d487023b
Akiyama Le <61172912+quanganhcf2@users.noreply.github.com>
2021-09-30 15:33:24 +0700

    Code Category create

---
+++ src/routes/categories.route.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import { Routes } from '@interfaces/routes.interface';
+import validationMiddleware from '@middlewares/validation.middleware';
+import CategoryController from '@/controllers/categories.controller';
+import { CreateCategoryDto } from '@/dtos/categories.dto';
+
+class CategoryRoute implements Routes {
+  public path = '/categories';
+  public router = Router();
+  public categoryController = new CategoryController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.post(
+      `${this.path}`,
+      validationMiddleware(CreateCategoryDto, 'body'),
+      this.categoryController.createCategory,
+    );
+  }
+}
+
+export default CategoryRoute;
